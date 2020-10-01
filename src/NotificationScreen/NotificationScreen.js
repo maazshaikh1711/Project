8d007126ad97ad10b5543eac4f1c35488bdceff3
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2020-10-01 19:10:22 +0530

    minor changes

--- src/NotificationScreen/NotificationScreen.js
+++ src/NotificationScreen/NotificationScreen.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    Button,
+    View, ScrollView,
+    Text, Image,
 } from 'react-native';
+import { Button, Banner, Card, Title, Paragraph } from 'react-native-paper';
+import { NotificationCard } from './NotificationCard';
 
 export const NotificationScreen = (navigation) => {
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>This is Notifications Screen.</Text>
-        </View>
+        <ScrollView>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', paddingTop: 15 }}>
+                <View style={{ height: '20%', width: '90%' }}>
+
+                    <NotificationCard title="Title" content="Description" />
+                    <NotificationCard title="Title" content="Description" />
+                    <NotificationCard title="Title" content="Description" />
+                    <NotificationCard title="Title" content="Description" />
+                    <NotificationCard title="Title" content="Description" />
+                    <NotificationCard title="Title" content="Description" />
+                    <NotificationCard title="Title" content="Description" />
+
+                </View>
+
+
+                {/* <Button mode="contained" >Press Me</Button> */}
+            </View >
+        </ScrollView>
     );
 }
