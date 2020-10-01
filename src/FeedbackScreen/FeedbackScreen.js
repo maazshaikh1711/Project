a124d24ae6ee553f6408e73036e910ea718a6ff4
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2020-10-01 19:11:24 +0530

    added feedback feature

--- src/FeedbackScreen/FeedbackScreen.js
+++ src/FeedbackScreen/FeedbackScreen.js
@@ -2,13 +2,38 @@ import React from 'react';
 import {
     View,
     Text,
-    Button,
 } from 'react-native';
 
+import { Button, Divider, TextInput } from 'react-native-paper';
+
 export const FeedbackScreen = (navigation) => {
+    const [text, setText] = React.useState('');
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>This is Feedback Screen.</Text>
-        </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', padding: 15 }}>
+            <Text style={{ fontSize: 30, fontStyle: 'italic', borderBottomWidth: 1 }}>We would like hear your valuable Feedback !</Text>
+            <View style={{ width: '100%', justifyContent: "center", paddingTop: 20 }}>
+                <TextInput
+                    mode="outlined"
+                    label="Your Feedback"
+                    value={text} onChangeText={text => setText(text)}
+                    selectionColor="#D3D3D3"
+                />
+            </View>
+            <View style={{ paddingTop: 20 }}>
+                <Button
+                    mode="contained"
+                    onPress={() => {
+                        console.log(text);
+                        setText('')
+                    }
+                    }
+                >
+                    Submit
+                </Button>
+            </View>
+        </View >
+
+
     );
 }
