9af9529f8631a802ed254cde1ef322ac3be9f669
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2020-10-01 19:31:32 +0530

    added dummy ratings feature

--- src/FeedbackScreen/FeedbackScreen.js
+++ src/FeedbackScreen/FeedbackScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
     View,
     Text,
+    Image,
 } from 'react-native';
 
 import { Button, Divider, TextInput } from 'react-native-paper';
@@ -10,30 +11,36 @@ export const FeedbackScreen = (navigation) => {
     const [text, setText] = React.useState('');
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', padding: 15 }}>
-            <Text style={{ fontSize: 30, fontStyle: 'italic', borderBottomWidth: 1 }}>We would like hear your valuable Feedback !</Text>
-            <View style={{ width: '100%', justifyContent: "center", paddingTop: 20 }}>
-                <TextInput
-                    mode="outlined"
-                    label="Your Feedback"
-                    value={text} onChangeText={text => setText(text)}
-                    selectionColor="#D3D3D3"
-                />
-            </View>
-            <View style={{ paddingTop: 20 }}>
-                <Button
-                    mode="contained"
-                    onPress={() => {
-                        console.log(text);
-                        setText('')
-                    }
-                    }
-                >
-                    Submit
+        <>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', padding: 15 }}>
+                <Text style={{ fontSize: 30, fontStyle: 'italic', borderBottomWidth: 1 }}>We would like hear your valuable Feedback !</Text>
+                <View style={{ width: '100%', justifyContent: "center", paddingTop: 20 }}>
+                    <TextInput
+                        mode="outlined"
+                        label="Your Feedback"
+                        value={text} onChangeText={text => setText(text)}
+                        selectionColor="#D3D3D3"
+                    />
+                </View>
+                <View style={{ paddingTop: 20 }}>
+                    <Button
+                        mode="contained"
+                        onPress={() => {
+                            console.log(text);
+                            setText('')
+                        }
+                        }
+                    >
+                        Submit
                 </Button>
+                </View>
             </View>
-        </View >
 
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
+                <Text style={{ fontSize: 30, fontStyle: 'italic', borderBottomWidth: 1 }}>Rate us now :)</Text>
+                <Image source={require('../assets/stars.png')} ></Image>
+            </View>
+        </>
 
     );
 }
