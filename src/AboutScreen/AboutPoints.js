39c72010ae876cb39ab9f6fc14218243d788211f
maazshaikh1711 <0maaz0shaikh0@gmail.com>
2020-11-01 13:23:07 +0530

    Styling in About Screen

--- src/AboutScreen/AboutPoints.js
+++ src/AboutScreen/AboutPoints.js
@@ -4,7 +4,7 @@ import { Paragraph } from 'react-native-paper';
 export const Points = (props) => {
     return (
         <Paragraph style={{ fontSize: 20, marginTop: 25 }}>
-            {props.description}
+            * {props.description}
         </Paragraph>
     );
 }
--- src/AboutScreen/AboutScreen.js
+++ src/AboutScreen/AboutScreen.js
@@ -15,16 +15,16 @@ export const AboutScreen = ({ navigation }) => {
             <View style={{ justifyContent: "center", alignItems: 'center' }}>
                 <Card style={{ width: "90%", marginTop: 15 }}>
                     <Card.Content>
-                        <Title style={{ fontSize: 30, color: "#5F00E3" }}>About App</Title>
-                        <Points description="- This App shows you nearby located towers." />
-                        <Points description="- This App is built by Amaan Shaikh and Rohan Rokade." />
-                        <Points description="- This App is built for our 7th Semester Project." />
-                        <Points description="- All other information can be written below here." />
+                        {/* <Title style={{ fontSize: 30, color: "#5F00E3" }}>About App</Title> */}
+                        <Points description="This App shows you nearby located towers." />
+                        <Points description="This App is built by Amaan Shaikh and Rohan Rokade." />
+                        <Points description="This App is built for our 7th Semester Project." />
+                        <Points description="All other information can be written below here." />
                     </Card.Content>
                 </Card>
-                <Ionicons name="map" size={50} color="red" onPress={() => navigation.openDrawer()} />
+                {/* <Ionicons name="map" size={50} color="red" onPress={() => navigation.openDrawer()} /> */}
             </View >
-            <View style={{ paddingTop: 15, alignItems: 'center' }}>
+            <View style={{ paddingTop: 20, alignItems: 'center' }}>
                 <Text >Copyright © 2020 by Amaan Shaikh. All rights reserved.</Text>
             </View>
         </ScrollView>
